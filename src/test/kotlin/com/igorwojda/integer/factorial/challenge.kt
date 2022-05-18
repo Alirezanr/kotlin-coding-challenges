c8c197c1cbb67e1ee4d4e922c0744c5800211529
Alireza Nori <alireza.nr1378@gmail.com>
2022-05-18 22:56:59 +0430

    Solved "Factorial".

--- src/test/kotlin/com/igorwojda/integer/factorial/challenge.kt
+++ src/test/kotlin/com/igorwojda/integer/factorial/challenge.kt
@@ -4,7 +4,24 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 private fun factorial(n: Int): Int {
-    TODO("not implemented")
+    //solution 1:
+    /*return when (n) {
+        0, 1 -> 1
+        else -> n * factorial(n - 1)
+    }*/
+
+
+    //solution 2:
+    if (n == 0)
+        return 1
+
+    var number = n
+    for (i in n downTo 1) {
+        if (i - 1 != 0)
+            number *= (i - 1)
+    }
+
+    return number
 }
 
 class RecursiveFactorial {
